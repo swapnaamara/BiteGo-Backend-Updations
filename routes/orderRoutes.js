0b90243aabf4ecdc42dd9902fff5f11e91c63ff9
swapnaamara <swapnaamara59@gmail.com>
2026-07-28 20:20:29 +0530

    Refactor orderRoutes.js for cleaner code structure

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,16 +8,12 @@ const {
 } = require('../controllers/orderController');
 const auth = require('../middleware/authMiddleware');
 
-// @route   POST /api/orders
 router.post('/', auth, placeOrder);
 
-// @route   GET /api/orders/myorders
 router.get('/myorders', auth, getMyOrders); // Customer
 
-// @route   GET /api/orders/restaurant
 router.get('/restaurant', auth, getRestaurantOrders); // Restaurant owner
 
-// @route   PUT /api/orders/:id/status
 router.put('/:id/status', auth, updateOrderStatus); // Restaurant/Rider/Admin
 
 module.exports = router;
