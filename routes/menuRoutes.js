aeb91a818301c4121278f3998554d526d3dda5ad
swapnaamara <swapnaamara59@gmail.com>
2026-07-28 20:19:46 +0530

    Refactor menuRoutes.js for cleaner syntax

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -8,16 +8,12 @@ const {
 } = require('../controllers/menuController');
 const auth = require('../middleware/authMiddleware');
 
-// @route   GET /api/menu/:restaurantId
 router.get('/:restaurantId', getMenuByRestaurant);
 
-// @route   POST /api/menu/:restaurantId
 router.post('/:restaurantId', auth, addMenuItem); // Restaurant owner
 
-// @route   PUT /api/menu/item/:id
 router.put('/item/:id', auth, updateMenuItem);
 
-// @route   DELETE /api/menu/item/:id
 router.delete('/item/:id', auth, deleteMenuItem);
 
 module.exports = router;
