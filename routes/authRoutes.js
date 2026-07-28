3f5cf509e7fc3f221e42390c039b66756249718d
swapnaamara <swapnaamara59@gmail.com>
2026-07-28 20:19:05 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const { register, login, getMe } = require('../controllers/authController');
 const auth = require('../middleware/authMiddleware');
 
-// @route   POST /api/auth/register
+
 router.post('/register', register);
 
-// @route   POST /api/auth/login
+
 router.post('/login', login);
 
-// @route   GET /api/auth/me
 router.get('/me', auth, getMe);
 
 module.exports = router;
